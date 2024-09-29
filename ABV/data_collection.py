113f6a3137c8daa7ac1085f65b4fbc23929fcb86
mattcattb <mboughton17@gmail.com>
2024-09-29 14:10:15 -0400

    heavy bug fixes to signal changes

--- ABV/data_collection.py
+++ ABV/data_collection.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import Jetson.GPIO as GPIO
 import cv2
 from nanocamera import Camera
@@ -11,20 +13,31 @@ from storage_utils import find_drive, create_new_folder
     Script for collecting image data and storing onto mounted data
 
 """
+
 cam = None
-b_led = 19 # data-collection running
+b_led = 21 # data-collection running
 
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(b_led, GPIO.OUT)
 
 def shutdown_process():
     global cam
-    cam.release() 
+    print("DC: shutting down cam and GPIO...")
+
+    print(f"cam: {cam}")
+    if cam is not None:
+        cam.release()
+        print("DC: cam released") 
+        
+    GPIO.setmode(GPIO.BOARD)
     GPIO.output(b_led, GPIO.LOW)
     GPIO.cleanup()
+    print("DC: gpios board released")
+
+    print("DC: GPIO cleanup complete")
 
 def shutdown_signal_handler(signum, frame):
-    print("Shutdown signal received...")
+    print("DC: Shutdown signal received, SIG:", signum)
     shutdown_process()
     sys.exit(0)  # Ensure the script exits after handling the signal
 
@@ -32,50 +45,47 @@ def shutdown_signal_handler(signum, frame):
 signal.signal(signal.SIGTERM, shutdown_signal_handler)
 signal.signal(signal.SIGINT, shutdown_signal_handler)
 
+fps = 30
 
-def run_data_collection(fps=30):
-    # collect image data with nanocamera
-    usb_location = find_drive()
-    
-    if usb_location is None:
-        print("ERROR: USB mount not found!")
-        return
-    
-    
-    folder_path = create_new_folder(usb_location, "data_storage")
-    print(f"Saving images to {folder_path}")
-    frame_count = 0
-    frame_delay = 1/fps
+# collect image data with nanocamera
+usb_location = find_drive()
 
-    # prepare camera for usage!
-    global cam
-    cam = Camera(camera_type=0, width=640, height=480, fps=30, enforce_fps=True, debug=True)
-    if not cam.isReady():
-        print("Camera could not be prepared...")
-        return
+if usb_location is None:
+    print("DC ERROR: USB mount not found!")
+    shutdown_process()
+    sys.exit(0)
 
-    print("Camera ready!")
-    
-    # turn on blue LED, everything is correct!
-    GPIO.output(b_led, GPIO.HIGH)    
-    
-    try:
-        while True:
-            frame = cam.read()
-            if frame is not None:
-                filename = f"{folder_path}/f{frame_count}.jpg"
-                cv2.imwrite(filename, frame)
-                frame_count += 1
-                time.sleep(frame_delay)
-                print(f"saved {filename}") 
-            else:
-                print("failed to get frame")
 
-    except KeyboardInterrupt:
-        print("Finished Data Collection")
-    finally:
-        print("Releasing camera!")
-        shutdown_process()         
+folder_path = create_new_folder(usb_location, "data_storage")
+print(f"DC: Saving images to {folder_path}")
+frame_count = 0
+frame_delay = 1/fps
+
+# prepare camera for usage!
+cam = Camera(camera_type=0, width=640, height=480, fps=30, enforce_fps=True, debug=True)
+if not cam.isReady():
+    print("DC ERROR: Camera could not be prepared...")
+    shutdown_process()
+    sys.exit(0)
+
+print("DC: Camera ready!")
+
+# turn on blue LED, everything is correct!
+GPIO.output(b_led, GPIO.HIGH)    
+
+while True:
+    frame = cam.read()
+    if frame is not None:
+        filename = f"{folder_path}/f{frame_count}.jpg"
+        try:
+            cv2.imwrite(filename, frame)
         
-if __name__ == "__main__":
-    run_data_collection()
+            frame_count += 1
+        
+            print(f"DC: saved {filename}") 
+        except Exception as e:
+            print(f"DC ERROR: Failed to save image: {e}")
+    else:
+        print("DC ERROR: failed to get frame")
+    time.sleep(frame_delay)       
+    
